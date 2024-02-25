9e8a17daff5ad8f77140438f5ce289230381e7b0
K.Balon <balonkg1@gmail.com>
2024-02-25 11:48:01 -0500

    Finalize comments.

--- src/life.c
+++ src/life.c
@@ -21,11 +21,31 @@ uint8_t buf[320 / 8 * 180];
  */
 
 
+/* 
+ * Note: found that using statics for 'bit' in set() & unset()
+ *  actually increased loop-time from 94.86 to 95.76-seconds.
+ */
+
+/* 
+ * Note: set() and unset operate at the bit-level, not byte level.
+ * When coupled with read-modify-write to set or clear a bit,
+ * this is computationally very inefficient.
+ * 
+ * Compare to initializing the screen in function setup(),
+ *  which is performed at the byte-level.
+ * Clearing the screen is computationally efficient.
+ * 
+ * Exercise: convert this program to byte-level manipulation using a 8bpp-screen.
+ * 
+ */
+
 /*static inline*/ void set(int x, uint8_t y)
 {
     uint8_t bit = 128 >> (x % 8);
 
     RIA.addr0 = (x / 8) + (320 / 8 * y);
+//  RIA.addr0 = (x / 8) + (40 * y);      //no advantage to pre-compute the 40
+
     RIA.step0 = 0;
 //  uint8_t bit = 128 >> (x % 8);
     RIA.rw0 |= bit;
@@ -36,6 +56,8 @@ uint8_t buf[320 / 8 * 180];
     uint8_t bit = 128 >> (x % 8);
 
     RIA.addr0 = (x / 8) + (320 / 8 * y);
+//  RIA.addr0 = (x / 8) + (40 * y);      //no advantage to pre-compute the 40
+
     RIA.step0 = 0;
 //  uint8_t bit = 128 >> (x % 8);
     RIA.rw0 &= ~bit;
@@ -60,8 +82,8 @@ uint8_t buf[320 / 8 * 180];
 
 //  xregn(1, 0, 0, 1, 2);
 //  xregn(1, 0, 1, 3, 3, 0, 0xff00);
-     xreg(1, 0, 0, 2);
-     xreg(1, 0, 1, 3, 0, 0xff00);
+     xreg(1, 0, 0, 2);            //2=320x180-canvas-dimension
+     xreg(1, 0, 1, 3, 0, 0xff00); //bitmapped, 1-bpp screen
 
     xram0_struct_set(0xFF00, vga_mode3_config_t, x_pos_px, 0);
     xram0_struct_set(0xFF00, vga_mode3_config_t, y_pos_px, 0);
@@ -71,12 +93,15 @@ uint8_t buf[320 / 8 * 180];
     xram0_struct_set(0xFF00, vga_mode3_config_t, xram_palette_ptr, 0xFFFF);
 
 #if 1
-    /* Initially clear the screen in XRAM.*/
+    /* Initialize the screen in XRAM.*/
     RIA.addr0 = 0;
     RIA.step0 = 1;
     for (/*unsigned*/ i = sizeof(buf); i--;)
-        RIA.rw0 = 0;
-#endif /*0*/
+        RIA.rw0 = 0xff;  //0=all-dead; 0xff=all-alive
+#endif
+//  Initial screen is now either all white or all black.
+
+    next(); // do one generation (~100-secs @ 8Mhz) on initial screen then place a glider.
 
     /* 
      * Initial condition: Glider
@@ -123,6 +148,19 @@ uint8_t buf[320 / 8 * 180];
              *  3. Any live cell with more than three live neighbors dies, as if by overpopulation.
              *  4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
              * 
+             *  5. (Implicit Rule):
+             *     Any dead cell with other than three live neighbors remains dead.
+             * 
+             * 
+             *  Reference: https://rosettacode.org/wiki/Conway's_Game_of_Life
+             * 
+             *  Cell  #Neighbors    new Cell
+             *    1   0,1             ->  0  # Lonely      - Rule#1
+             *    1   2,3             ->  1  # Lives       - Rule#2
+             *    1   4,5,6,7,8       ->  0  # Overcrowded - Rule#3
+             *    0   3               ->  1  # It takes three to give birth! - Rule#4
+             *    0   0,1,2,4,5,6,7,8 ->  0  # Barren      - Rule#5
+             * 
              */
 
             /*uint8_t*/ neighbors =
@@ -147,8 +185,10 @@ uint8_t buf[320 / 8 * 180];
                     set(x, y); //birth
             }
 
-              // Rule #2 - Any live cell with two or three live neighbors lives on to the next generation.
-              /* Sustain life - do nothing */
+              // Rules #2 & #5 
+              //  Any live cell with two or three live neighbors lives on to the next generation.
+              //  Any dead cell with other than three live neighbors remains dead.
+              /*  - do nothing */
 
         } //end for(y)
     } // end for(x)
@@ -165,12 +205,13 @@ void main(void)
     printf("Setting up screen for Life\n");
     setup();
 
-    for (/*int*/ i = 500; i>0; i--)
+    for (i=0; 1; i++)    
+ // for (/*int*/ i = 500; i>0; i--)
     {
 
         clock_now = clock();
-        if (i==500) clock_old = clock_now;
-        printf("loop i= %d clock()= 0x%lx delta=%ld \n", i, clock_now, (clock_now - clock_old) );
+        if (i==0) clock_old = clock_now;
+        printf("loop i= %d clock()= 0x%06.6lx delta=%ld \n", i, clock_now, (clock_now - clock_old) );
 
         next();
         clock_old = clock_now;
